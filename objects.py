90d5c3f1299413817a94e79624b366622db7a432
Maria Jose Sanchez Cairazco <mariajose.sanchezcairazco@gmail.com>
2025-09-21 19:07:00 -0500

    Went over type of objects and common type and attribute errors

--- objects.py
+++ objects.py
@@ -1,1 +1,24 @@
 # Object Oriented Programming in Python
+
+def hello():
+  print("hello")
+
+x = 1
+print(type(hello))      # <class 'function'>
+print(type("hello"))    # <class 'str'>
+print(type(x))          # <class 'int'>
+
+# Common errors
+
+y = 1
+z = "hello"
+
+# print(y + z)          # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+###
+
+string = "hello"
+x = 1
+
+print(string.upper())   # "HELLO"
+# print(x.upper())      # AttributeError: 'int' object has no attribute 'upper'
