bab14e16bb88ac43473cee7c043fed636dec2fc0
Maria Jose Sanchez Cairazco <mariajose.sanchezcairazco@gmail.com>
2025-10-20 23:47:40 -0500

    Class Attributes

---
+++ class_attributes.py
@@ -0,0 +1,20 @@
+# Object Oriented Programming in Python: Class Attributes
+
+class Person:
+  number_of_people = 0
+
+  def __init__(self, name):
+    self.name = name
+    Person.add_person()
+
+  @classmethod
+  def number_of_people_(cls):
+    return cls.number_of_people
+  
+  @classmethod
+  def add_person(cls):
+    cls.number_of_people += 1
+
+p1 = Person("Tim")
+p2 = Person("Jill")
+print(Person.number_of_people_())
